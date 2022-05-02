8ed99e6e111597fb045891f4d9aac4ea54bbb1e5
ShreeshaVoniyadka <shreeshavoniyadka@gmail.com>
2022-05-02 16:57:16 +0530

    Why the hell did i do that

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
-import os
-from flask import Flask, abort, render_template,request
+from unittest import result
+from flask import Flask, render_template,request
 import requests
 app = Flask(__name__)
-secretkey = "5657ec52b9ce3c0254b375c7ac428f8b"
+secretkey = "fbf2c29ad43f404ca0e110825220205"
 TEMPLATES_AUTO_RELOAD = True
+
 @app.route("/")
 def main():
     return render_template("Home.html")
+
 def kelvintocelcius(degree):
     C = degree - 273.15
     return int(C)
+
 @app.route("/",methods=['GET','POST'])
 def templatedisplay():    
     if request.method == "POST":
         city = request.form['city']
     else:
         city = "Bengaluru"
-    try:
-        location = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={secretkey}").json()
-    except:
-        return abort("Enter valid city")
-    loca = {}
-    for d in location:
-       loca.update(d) 
-    lat = loca['lat']
-    lon = loca['lon']
-    try:
-        result = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={secretkey}").json()
-    except:
-        return abort("Enter valid city")
-    incelcius = kelvintocelcius(result['main']['temp'])
-    result = {
-        "temp": str(incelcius) + 'Degree',
-        "pressure": str(result['main']['pressure']),
-        "humidity": str(result['main']['humidity']),
-    }
+    result = requests.get(f"https://api.weatherapi.com/v1/current.json?key={secretkey}&q={city}&aqi=yes").json()
 
     return render_template("Home.html",result=result)
 
